11e289eeed444556f2c1ef2ab1a46a90a65a181f
nagi-moriyama-lvgs <nagi.moriyama@leverages.jp>
2021-06-22 08:35:41 +0900

    目次のクリックイベントを設定

--- js/article.js
+++ js/article.js
@@ -1,52 +1,58 @@
-window.addEventListener('DOMContentLoaded', ()=>{
-  if (!document.querySelector('.article-detail')){ return false;}
+window.addEventListener("DOMContentLoaded", () => {
+  if (!document.querySelector(".article-detail")) {
+    return false;
+  }
   // 目次の作成
-  const tableOfContent = document.querySelector('.article-table');
-  const tableOfContentSide = document.querySelector('.article-table-side')
-  const titleDOMs      = Array.from(document.querySelectorAll('h2,h3'));
+  const tableOfContent = document.querySelector(".article-table");
+  const tableOfContentSide = document.querySelector(".article-table-side");
+  const titleDOMs = Array.from(document.querySelectorAll("h2,h3"));
 
-  titleDOMs.forEach((title, index)=>{
+  titleDOMs.forEach((title) => {
     const rank = Number(title.tagName.substring(1));
-    let tableContent = '';
-    if (rank === 2){
-      tableContent += `<div class="article-table-title">${title.textContent}</div>`
-    }else if(rank === 3){
-      tableContent += `<div class="article-table-title sub">${title.textContent}</div>`
-    }else{}
-    tableOfContent.insertAdjacentHTML('beforeend',tableContent);
-    tableOfContentSide.insertAdjacentHTML('beforeend',tableContent);
-
-    const tableContentDOM = document.querySelectorAll('.article-table-title:last-child')[0];
-    tableContentDOM.addEventListener('click',(e)=>{
-      scrollTo(0, window.pageYOffset + title.getBoundingClientRect().top - 100);
-      e.preventDefault();
-      e.stopPropagation();
-    })
+    const tableContent = document.createElement("div");
+    rank === 2 && tableContent.setAttribute("class", "article-table-title");
+    rank === 3 && tableContent.setAttribute("class", "article-table-title sub");
+    tableContent.innerText = title.textContent;
+    sideTableContent = tableContent.cloneNode(true);
+    const addScrollEvent = (target, title) => {
+      target.addEventListener("click", (e) => {
+        scrollTo(0, window.pageYOffset + title.getBoundingClientRect().top - 100);
+        e.preventDefault();
+        e.stopPropagation();
+      });
+    }
+    addScrollEvent(tableContent, title);
+    addScrollEvent(sideTableContent, title);
+    tableOfContent.appendChild(tableContent);
+    tableOfContentSide.appendChild(sideTableContent);
   });
 
   // サイド目次の表示制御
-  window.addEventListener('scroll',()=>{
+  window.addEventListener("scroll", () => {
     const scroll = window.pageYOffset;
     const windowHeight = window.innerHeight;
-    const sideHeight = document.querySelector('.side').getBoundingClientRect().top;
-    if ( scroll < windowHeight ){
-      tableOfContentSide.style.display = 'none';
-    }else if( sideHeight < windowHeight ){
-      tableOfContentSide.style.display = 'none';
-    }else{
-      tableOfContentSide.style.display = 'block';
+    const sideHeight = document
+      .querySelector(".side")
+      .getBoundingClientRect().top;
+    if (scroll < windowHeight) {
+      tableOfContentSide.style.display = "none";
+    } else if (sideHeight < windowHeight) {
+      tableOfContentSide.style.display = "none";
+    } else {
+      tableOfContentSide.style.display = "block";
     }
   });
 
   // サイド目次の初期表示
-  const firstTableOfContentSideDisplay = ()=>{
+  const firstTableOfContentSideDisplay = () => {
     const scroll = window.pageYOffset;
     const windowHeight = window.innerHeight;
-    const sideHeight = document.querySelector('.side').getBoundingClientRect().top;
-    if( scroll < windowHeight || sideHeight < windowHeight ){
-      tableOfContentSide.style.display = 'none';
+    const sideHeight = document
+      .querySelector(".side")
+      .getBoundingClientRect().top;
+    if (scroll < windowHeight || sideHeight < windowHeight) {
+      tableOfContentSide.style.display = "none";
     }
   };
   firstTableOfContentSideDisplay();
-
 });
